2991053f2b084855fe6b3dcea5bdc60dbbd1e322
Akanksha Pathak <pathakakanksha1289@gmail.com>
2026-08-12 21:52:42 +0530

    eleventh commit

---
+++ client/src/pages/Movies.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {dummyShowsData} from '../assets/assets'
+import MovieCard from '../components/MovieCard'
+import BlueCircle from '../components/BlueCircle'
+
+const Movies = () => {
+  return dummyShowsData.length > 0 ? (
+    <div className='relative my-40 mb-50 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
+       
+      <BlueCircle top='150px' left='0px'/>
+      <BlueCircle bottom='50px' right='50px'/>  
+
+      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
+      <div className='flex flex-wrap max-sm:justify-center gap-8'>
+          {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </div>
+  ) : (
+    <div>
+      <h1 className='text-3xl font-bold text-center'>No Movies Found</h1>
+    </div>
+  )
+}
+
+export default Movies
